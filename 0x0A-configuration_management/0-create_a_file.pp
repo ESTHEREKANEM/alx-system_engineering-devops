d4bf05395131c5b5e70c2a41a397043f71752e00
ESTHEREKANEM <estekanem1@gmail.com>
2023-03-24 14:52:43 -0700

    Puppet is fun

---
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -0,0 +1,9 @@
+# create a file in /tmp with the specific requirements:
+
+file { 'school':
+  path    => '/tmp/school',
+  mode    => '0744',
+  owner   => 'www-data',
+  group   => 'www-data',
+  content => 'I love Puppet',
+}
