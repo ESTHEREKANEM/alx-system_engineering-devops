b2a4f4c7f291a393c2266c56ee3bcc1b58a3ac2f
ESTHEREKANEM <estekanem1@gmail.com>
2023-04-09 13:19:15 -0700

    Task 2

--- 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -1,34 +1,17 @@
 #!/usr/bin/env bash
-# We automate the task of creating a custom HTTP header response, but with Puppet.
+# We will automate the task by creating a custom HTTP header response, but with Puppet.
 
-sudo apt-get update
-sudo apt-get install puppet-agent
-
-lass apache {
-  package { 'apache2':
-    ensure => installed,
-  }
-
-  service { 'apache2':
-    ensure  => running,
-    enable  => true,
-    require => Package['apache2'],
-  }
-
-  file { '/var/www/html/index.html':
-    ensure  => file,
-    content => "<html><head><title>Hello World</title></head><body><h1>Hello World!</h1></body></html>",
-    require => Package['apache2'],
-  }
-
-  file { '/etc/apache2/sites-available/000-default.conf':
-    ensure  => file,
-    content => "# Custom Apache configuration\n<VirtualHost *:80>\n    ServerName example.com\n    ServerAdmin webmaster@localhost\n    DocumentRoot /var/www/html\n    ErrorLog ${APACHE_LOG_DIR}/error.log\n    CustomLog ${APACHE_LOG_DIR}/access.log combined\n</VirtualHost>\n",
-    require => Package['apache2'],
-    notify  => Service['apache2'],
-  }
+exec {'update':
+  command => '/usr/bin/apt-get update',
 }
-
-sudo puppet apply /etc/puppetlabs/code/environments/production/manifests/apache.pp
-
-
+-> package {'nginx':
+  ensure => 'present',
+}
+-> file_line { 'http_header':
+  path  => '/etc/nginx/nginx.conf',
+  match => 'http {',
+  line  => "http {\n\tadd_header X-Served-By \"${hostname}\";",
+}
+-> exec {'run':
+  command => '/usr/sbin/service nginx restart',
+}
