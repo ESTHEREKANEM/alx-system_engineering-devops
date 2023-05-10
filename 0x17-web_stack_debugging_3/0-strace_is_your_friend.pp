953ca85c5d74087a8853e7d19bb3b4ef2bea1ea9
ESTHEREKANEM <estekanem1@gmail.com>
2023-05-10 13:50:02 -0700

    task 0

---
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -0,0 +1,8 @@
+# A puppet manuscript that replaces a line in a file on my server
+
+$file_to_edit = '/var/www/html/wp-settings.php'
+
+exec { 'replace_line':
+  command => "sed -i 's/phpp/php/g' ${file_to_edit}",
+  path    => ['/bin','/usr/bin']
+}
