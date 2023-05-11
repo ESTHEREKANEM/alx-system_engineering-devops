6fffee0044e2e9da78640945112ec3395f9e0f44
ESTHEREKANEM <estekanem1@gmail.com>
2023-05-11 14:08:42 -0700

    Update Task 0

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -8,12 +8,12 @@ def number_of_subscribers(subreddit)
     """This function queries the Reddit API"""
 
     url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
-    headers = {"User-Agent": "MyRedditAPI_project")
-    response = requests.get(url, headers=headers, allow_redirects=False)
+    headers = {
+            "User-Agent": "MyRedditAPI_project")
+    }
 
-    if response.status_code == 200:
-        data = response.json()
-        no_subs = data["data"]["subscribers"]
-        return no_subs
-    else:
+    response = requests.get(url, headers=headers, allow_redirects=False)
+    if response.status_code == 404:
         return 0
+    results = response.json().get("data")
+    return results.get("subscribers")
